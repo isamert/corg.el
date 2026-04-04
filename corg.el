5e548573fd8895a8ee4260c7f8b2e4893546fad6
Isa Mert Gurbuz <isamertgurbuz@gmail.com>
2026-04-04 23:59:57 +0200

    Add usage section, fix version

--- corg.el
+++ corg.el
@@ -4,7 +4,7 @@
 
 ;; Author: Isa Mert Gurbuz <isamertgurbuz@gmail.com>
 ;; URL: https://github.com/isamert/corg.el
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1") (s "1.13.1") (compat "30"))
 ;; Keywords: abbrev convenience completion matching
 
@@ -27,10 +27,19 @@
 
 ;; *corg.el* (as in *C*omplete *ORG*) is an Emacs package that
 ;; provides completion-at-point for Org-mode source block and dynamic
-;; block headers.  /corg/ auto-completes programming language names,
+;; block headers.  corg auto-completes programming language names,
 ;; their parameters, and possible parameter values for source blocks.
 ;; For dynamic blocks, it completes block names, their parameters, and
-;; possible parameter values.
+;; possible parameter values.  It also completes block names inside
+;; noweb references and in #+call syntax.
+
+;;;; Usage
+
+;; To enable corg in current Org buffer, do M-x `corg-setup' or to
+;; automatically enable it on Org mode buffers by adding a hook use
+;; the following:
+;;
+;;   (add-hook \\='org-mode-hook #\\='corg-setup)
 
 ;;; Code:
 
@@ -63,14 +72,14 @@ See `corg' for detailed documentation."
        (mapcar #'car candidates)
        :annotation-function
        (lambda (candidate)
-	 (concat " " (plist-get (alist-get candidate candidates nil nil #'equal) :ann)))
+         (concat " " (plist-get (alist-get candidate candidates nil nil #'equal) :ann)))
        :company-doc-buffer
        (lambda (candidate)
-	 (with-current-buffer (get-buffer-create " *corg*")
+         (with-current-buffer (get-buffer-create " *corg*")
            (erase-buffer)
            (let ((doc (plist-get (alist-get candidate candidates nil nil #'equal) :doc)))
              (insert (if (functionp doc)
-			 (funcall doc)
+                         (funcall doc)
                        doc)))
            (current-buffer)))))))
 
